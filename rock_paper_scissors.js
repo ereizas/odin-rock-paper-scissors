fa898a389be2138cda90634e9cff621b23e2153f
ereizas <ereizas@gmail.com>
2025-06-29 14:54:53 -0400

    Run round of game

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -24,3 +24,20 @@ function getHumanChoice(){
             return "scissors";
     }
 }
+
+function playRound(humanChoice,computerChoice){
+    beats = {"rock":{"paper":false,"scissors":true},"paper":{"rock":true,"scissors":false},"scissors":{"rock":false,"paper":true}}
+    if(humanChoice==computerChoice){
+        console.log("Tie");
+    }
+    else if(beats[humanChoice][computerChoice]){
+        humanScore++;
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    }
+    else{
+        computerScore++;
+        console.log(`You lose. ${computerChoice} beats ${humanChoice}`)
+    }
+}
+
+playRound(getHumanChoice(),getComputerChoice());
